d76382ff4ee84501253387c9a11c16228a05e549
Raushan-Shahi <raushanshahi2001@gmail.com>
2023-06-22 16:48:41 +0530

    factorial

--- math_factorial.cpp
+++ math_factorial.cpp
@@ -1,11 +1,25 @@
 #include <iostream>
 #include <bits/stdc++.h>
 using namespace std;
-    
 
+int fact(int n)
+{
+    int res = 1;
+    for (int i = 2; i <= n; i++)
+        res = res * i;
+    return res;
+}
 
-    
-int main(){
-    
-return 0;
+int factorial(int n)
+{
+    if (n == 0)
+        return 1;
+    return n * factorial(n - 1);
+}
+
+int main()
+{
+    cout << fact(5) << endl;
+    cout << factorial(6);
+    return 0;
 }
