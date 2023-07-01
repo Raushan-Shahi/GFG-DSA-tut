4842f627dbf580c8028a4a247a6e3845eb354db7
Raushan-Shahi <raushanshahi2001@gmail.com>
2023-07-01 14:50:57 +0530

    recursion generate subset

---
+++ recursion_generate_subset.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+    
+void subset(string s, string current="", int i=0){
+    if(i==s.length())
+    {
+        cout<< current <<endl;
+        return;
+    }
+    subset(s,current,i+1);
+    subset(s,current+s[i],i+1);
+}
+
+
+
+int main(){
+    subset("ABC", "",0);
+    return 0;
+}
