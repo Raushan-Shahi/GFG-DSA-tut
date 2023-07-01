0d3e6a2408de512d7647f6bf463084bcd036626a
Raushan-Shahi <raushanshahi2001@gmail.com>
2023-07-01 12:34:54 +0530

    factorial

--- math_factorial.cpp
+++ math_factorial.cpp
@@ -1,28 +1,18 @@
-// theta(n) time complexity for recursive solution 
-
-
-#include <iostream>
 #include <bits/stdc++.h>
+
 using namespace std;
 
-int fact(int n)
-{
-    int res = 1;
-    for (int i = 2; i <= n; i++)
-        res = res * i;
-    return res;
-}
+/*
+ * Complete the 'FindTheSequence' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts INTEGER n as parameter.
+ */
 
 int factorial(int n)
 {
-    if (n == 0)
+    if(n==0)
         return 1;
-    return n * factorial(n - 1);
+    return n*factorial(n-1);
 }
 
-int main()
-{
-    cout << fact(5) << endl;
-    cout << factorial(6);
-    return 0;
-}
