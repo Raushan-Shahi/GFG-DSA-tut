bad0c5e60de3d253e85ee78898ee3ea68501ca7b
Raushan-Shahi <raushanshahi2001@gmail.com>
2023-07-11 14:02:54 +0530

    searching binary recurssion

--- searching_binary_recursision.cpp
+++ searching_binary_recursision.cpp
@@ -1,30 +1,29 @@
 #include <iostream>
 #include <bits/stdc++.h>
 using namespace std;
-    
 
 int binsearch(int arr[], int low, int high, int x)
 {
-    if(low > high)
+    if (low > high)
         return -1;
-    int mid = (low + high)/2;
+    int mid = (low + high) / 2;
 
-    if(arr[mid] == x)
+    if (arr[mid] == x)
         return mid;
 
-    else if(arr[mid] > x)
-        return binsearch(arr,low, mid -1, x);
-    
+    else if (arr[mid] > x)
+        return binsearch(arr, low, mid - 1, x);
+
     else
-        return binsearch(arr, mid+1, high, x);
+        return binsearch(arr, mid + 1, high, x);
 }
 
+int main()
+{
+    int arr[] = {1, 2, 3, 4, 5, 6, 7, 8}, n = 8;
+    int x = 3;
 
-int main(){
-    int arr[]={1,2,3,4,5,6,7,8}, n =8;
-    int x =3;
-
-    cout << binsearch(arr,0,n-1,x);
+    cout << binsearch(arr, 0, n - 1, x);
     return 0;
-return 0;
+    return 0;
 }
