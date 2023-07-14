e05515289e7de25b1f535144879d10182e8a1f11
Raushan-Shahi <raushanshahi2001@gmail.com>
2023-07-14 16:14:47 +0530

    sum of n numbers using backtracking

---
+++ recursionn_sum_n_numbers.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+    
+
+void summation(int i, int sum)
+{
+    if(i<1){
+        cout << sum;
+        return;
+    }
+    summation(i-1, sum+i);
+}
+
+
+
+int main(){
+    summation(10,0);
+return 0;
+}
