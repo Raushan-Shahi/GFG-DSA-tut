530fa68802ca34d045e50c8870c82706f5ec8692
Raushan-Shahi <raushanshahi2001@gmail.com>
2023-08-05 17:15:20 +0530

    bubble sort

--- sort_bubbl_sort.cpp
+++ sort_bubbl_sort.cpp
@@ -9,10 +9,10 @@ using namespace std;
 void bubble_sort( int *arr, int n){
     for (int i = 0; i < n-1; i++)
     {
-        for (int j = i; j < n; j++)
+        for (int j = 0; j < n-i-1; j++)
         {
-            if(arr[i]>arr[j])
-                swap(arr[i],arr[j]);
+            if(arr[j]>arr[j+1])
+                swap(arr[j],arr[j+1]);
         }
     }
 }
